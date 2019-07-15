999a2f4980fa902ebe1acbbc230d06e0b0a2d368
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-15 21:49:03 +0600

    ৩২.৬ - রিপিটেবল গ্রুপের নেস্টিং - অর্থাৎ গ্রুপের মাঝে গ্রুপ
    ৩২.৬ - রিপিটেবল গ্রুপের নেস্টিং - অর্থাৎ গ্রুপের মাঝে গ্রুপ

---
+++ app/public/wp-content/themes/twentyseventeen/piklist/parts/meta-boxes/team.php
@@ -0,0 +1,57 @@
+<?php
+
+/*
+Title: Team
+Post Type: post
+*/
+
+
+piklist('field',array(
+	'type' => 'group',
+	'field' => 'team_members',
+	'label' => __('Team Members','twentyseventeen'),
+	'add_more' => true,
+	'fields' => array(
+		array(
+			'type' => 'text',
+			'field' => 'team_member_name',
+			'label' => __('Name','twentyseventeen'),
+		),
+
+		array(
+			'type' => 'group',
+			'field' => 'team_member_testimonials',
+			'label' => __('Testimonials','twentyseventeen'),
+			'add_more' => true,
+			'fields' => array(
+				array(
+					'type' => 'text',
+					'field' => 'tname',
+					'label' => __('Name','twentyseventeen'),
+					'attributes' => array(
+						'class' => 'widefat'
+					)
+				),
+
+				array(
+					'type' => 'file',
+					'field' => 'timage',
+					'label' => __('Image','twentyseventeen'),
+					'attributes' => array(
+						'class' => 'widefat'
+					)
+				),
+
+				array(
+					'type' => 'text',
+					'field' => 'description',
+					'label' => __('Description','twentyseventeen'),
+					'attributes' => array(
+						'class' => 'widefat'
+					)
+				)
+
+			)
+		)
+	)
+));
