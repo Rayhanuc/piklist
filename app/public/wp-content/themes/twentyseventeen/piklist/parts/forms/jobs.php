998495ab0fe19588960e350f1bfe29dfc6b1bd49
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-20 11:50:33 +0600

    ৩২.১৭ - পিকলিস্ট দিয়ে ট্যাক্সোনমির জন্য মেটা ফিল্ড তৈরী করা
    ৩২.১৭ - পিকলিস্ট দিয়ে ট্যাক্সোনমির জন্য মেটা ফিল্ড তৈরী করা

--- app/public/wp-content/themes/twentyseventeen/piklist/parts/forms/jobs.php
+++ app/public/wp-content/themes/twentyseventeen/piklist/parts/forms/jobs.php
@@ -1,60 +1,59 @@
 <?php
 
-/* 
+/*
 Title: Job Application
 Method: post
 Message: Your Job Application Has Been Received
 Logged in: false
  */
 
-
- piklist('field',array(
+piklist('field', array(
     'type'  => 'hidden',
     'field' => 'post_type',
     'value' => 'job',
-    'scope' => 'post'
- ));
+    'scope' => 'post',
+));
 
- piklist('field',array(
+piklist('field', array(
     'type'  => 'hidden',
     'field' => 'post_status',
     'value' => 'pending',
-    'scope' => 'post'
- ));
+    'scope' => 'post',
+));
 
- piklist('field',array(
+piklist('field', array(
     'type'  => 'text',
     'field' => 'post_title',
     'scope' => 'post',
-    'label' => __('Your Name', 'twentyseventeen')
- ));
+    'label' => __('Your Name', 'twentyseventeen'),
+));
 
- piklist('field',array(
-    'type'  => 'textarea',
-    'field' => 'post_content',
-    'scope' => 'post',
-    'label' => __('Biography', 'twentyseventeen'),
+piklist('field', array(
+    'type'       => 'textarea',
+    'field'      => 'post_content',
+    'scope'      => 'post',
+    'label'      => __('Biography', 'twentyseventeen'),
     'attributes' => array(
-        'style: height:200px;'
-    )
- ));
+        'style: height:200px;',
+    ),
+));
 
- piklist('field',array(
+piklist('field', array(
     'type'  => 'text',
     'field' => 'email',
     'scope' => 'post_meta',
-    'label' => __('Email', 'twentyseventeen')
- ));
+    'label' => __('Email', 'twentyseventeen'),
+));
 
- piklist('field',array(
+piklist('field', array(
     'type'  => 'text',
     'field' => 'phone',
     'scope' => 'post_meta',
-    'label' => __('Phone Number', 'twentyseventeen')
- ));
+    'label' => __('Phone Number', 'twentyseventeen'),
+));
 
- piklist('field',array(
+piklist('field', array(
     'type'  => 'submit',
     'field' => 'submit',
-    'value' => __('Apply', 'twentyseventeen')
- ));
+    'value' => __('Apply', 'twentyseventeen'),
+));
---
+++ app/public/wp-content/themes/twentyseventeen/piklist/parts/terms/terms-meta.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+Title: Terms Extra Fields
+Taxonomy: category, post_tag
+ */
+?>
+
+<p>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed fringilla arcu ante, ut lobortis ex porttitor in. Nam at malesuada felis. Aliquam ornare cursus urna, at elementum est scelerisque malesuada.
+</p>
+
+<?php
+
+piklist('field', array(
+    'type'       => 'text',
+    'field'      => 'extra_data',
+    'label'      => __('Some Extra Data', 'twentyseventeen'),
+    'help'       => __('Here is some help', 'twentyseventeen'),
+    'attributes' => array(
+        'class' => 'large-text',
+    ),
+));
+
+piklist('field', array(
+    'type'    => 'file',
+    'field'   => 'category_image',
+    'label'   => __('Category Image', 'twentyseventeen'),
+    'help'    => __('Here is some help', 'twentyseventeen'),
+    'options' => array(
+        'model_title' => __('Category Image', 'twentyseventeen'),
+        'help'        => __('Add', 'twentyseventeen'),
+    ),
+));
