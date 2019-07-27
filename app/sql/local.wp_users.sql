3ab28982e0cdc1d0692f901d7e98bbfd2fce34eb
Md. Rayhan Uddin Chowdhury <30935802+Rayhanuc@users.noreply.github.com>
2019-07-27 19:06:04 +0600

    some updated of flywheel

--- app/sql/local.wp_users.sql
+++ app/sql/local.wp_users.sql
@@ -2,4 +2,6 @@
 /*!40014 SET FOREIGN_KEY_CHECKS=0*/;
 /*!40103 SET TIME_ZONE='+00:00' */;
 INSERT INTO `wp_users` VALUES
-(1,"piklisttest","$P$BR7L1tbahaYRRlYktIOZG2lR3u.yih/","piklisttest","dev-email@flywheel.local","","2019-07-12 16:48:49","",0,"piklisttest");
+(1,"piklisttest","$P$BR7L1tbahaYRRlYktIOZG2lR3u.yih/","piklisttest","dev-email@flywheel.local","","2019-07-12 16:48:49","",0,"piklisttest"),
+(2,"jamal","$P$BSr/7o5UjPlh5GsDooQitcQB/le4yn/","jamal","jamal@uddin.com","","2019-07-21 10:02:47","1563703367:$P$B85d2g/IfQAPwRjEAp2020UEmY2KQ61",0,"Jamal Uddin"),
+(3,"korim","$P$B85g5Pg46kN8MfCoXNDW6Q0uAIOBxI.","korim","korim@uddin.com","","2019-07-21 10:03:39","1563703419:$P$BFePzPNbKLHklMrTO03ZDLeqyMgSOu1",0,"Korim Uddin");
